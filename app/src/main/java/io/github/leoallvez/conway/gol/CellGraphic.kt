a13837387315a79d2eb9fcb59b12cf21d306f720
leoallvez <leoallvez@hotmail.com>
2021-06-05 05:23:34 -0300

    game logic

--- app/src/main/java/io/github/leoallvez/conway/gol/CellGraphic.kt
+++ app/src/main/java/io/github/leoallvez/conway/gol/CellGraphic.kt
@@ -1,5 +1,6 @@
 package io.github.leoallvez.conway.gol
 
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
@@ -9,14 +10,26 @@ class CellGraphic(private val columnWidth: Int, private val rowHeight: Int) {
     val rectangle: Rect = Rect()
     val paint: Paint = Paint()
 
-    fun draw(col: Int, row: Int, cellIsAlive: Boolean) {
+    fun draw(col: Int, row: Int, cellIsAlive: Boolean, canvas: Canvas) {
         rectangle.set(
             getRectangleLeft(col),
             getRectangleTop(row),
             getRectangleRight(col),
             getRectangleBottom(row)
         )
+        //paint.style = Paint.Style.STROKE;
+        //paint.color = getCellColor(cellIsAlive)
+
+        // fill
+        paint.style = Paint.Style.FILL
+        //paint.setColor(Color.MAGENTA);
         paint.color = getCellColor(cellIsAlive)
+        canvas.drawRect(rectangle, paint);
+
+        // border
+        paint.style = Paint.Style.STROKE
+        paint.color = Color.BLACK
+        canvas.drawRect(rectangle, paint)
     }
 
     private fun getCellColor(cellIsAlive: Boolean): Int = if (cellIsAlive) {
--- app/src/main/java/io/github/leoallvez/conway/gol/GameOfLifeView.kt
+++ app/src/main/java/io/github/leoallvez/conway/gol/GameOfLifeView.kt
@@ -1,5 +1,7 @@
 package io.github.leoallvez.conway.gol
 
+import android.R.attr.columnWidth
+import android.R.attr.rowHeight
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
@@ -8,6 +10,7 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.SurfaceView
 import kotlin.random.Random
+
 
 class GameOfLifeView(context: Context?, attrs: AttributeSet?) :
     SurfaceView(context, attrs),
@@ -23,6 +26,8 @@ class GameOfLifeView(context: Context?, attrs: AttributeSet?) :
     private var columns = 1
     private var rows = 1
 
+    private lateinit var universe: Universe
+
     init {
         initWorld()
     }
@@ -30,11 +35,12 @@ class GameOfLifeView(context: Context?, attrs: AttributeSet?) :
     override fun run() {
         while (isRunning) {
             try {
-                Thread.sleep(200)
+                Thread.sleep(300)
             } catch (e: InterruptedException) {
                 Log.e("error:", "${e.printStackTrace()}")
             }
             val canvas: Canvas = holder.lockCanvas()
+            universe.nextGeneration()
             drawCells(canvas)
             holder.unlockCanvasAndPost(canvas)
         }
@@ -62,6 +68,7 @@ class GameOfLifeView(context: Context?, attrs: AttributeSet?) :
         calculateColumnsRowsNumber(metrics)
         val (columnWidth, rowHeight) = calculateColumnWidthRowHeight(metrics)
         cellGraphic = CellGraphic(columnWidth, rowHeight)
+        universe = Universe(columns, rows)
     }
 
     private fun getDisplayMetrics(): DisplayMetrics {
@@ -86,19 +93,30 @@ class GameOfLifeView(context: Context?, attrs: AttributeSet?) :
     private fun drawCells(canvas: Canvas) {
         for (col in 0 until columns) {
             for (row in 0 until rows) {
-                val cellIsAlive = (Random.nextInt(0, 10) % 2 == 0)
-                cellGraphic.draw(col, row, cellIsAlive)
-                canvas.drawRect(cellGraphic.rectangle, cellGraphic.paint)
+                val cellIsAlive = universe.getCell(col, row).isAlive
+                //val cellIsAlive = (Random.nextInt(0, 10) % 2 == 0)
+                cellGraphic.draw(col, row, cellIsAlive, canvas)
+                //canvas.drawRect(cellGraphic.rectangle, cellGraphic.paint)
             }
         }
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         Log.d("test", "We is here!")
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            // we get the coordinates of the touch and we convert it in coordinates for the board
+            val i = (event.x / columnWidth).toInt()
+            val j = (event.y / rowHeight).toInt()
+            // we get the cell associated to these positions
+            val cell: Cell = universe.getCell(i, j)
+            // we call the invert method of the cell got to change its state
+            cell.isAlive = true
+            //invalidate()
+        }
         return super.onTouchEvent(event)
     }
 
     companion object {
-        const val DEFAULT_SIZE = 50
+        const val DEFAULT_SIZE = 10
     }
 }
